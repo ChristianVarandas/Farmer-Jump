7d0f1786b3efc96c341c159bd7535d1206a8de7e
GuilhermeHLSouza <guilherme.souza726@etec.sp.gov.br>
2024-03-29 11:30:45 -0300

    adicionando animação e o feno

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const farmer = document.querySelector(".farmer") //criando a variavel farmer 
-const feno = document.querySelector(".feno") //criando a variavel feno (provavelmente será alterado depois)
-
+const feno = document.querySelector(".feno")//criando a variavel feno (provavelmente será alterado depois)
+let animacao = 1
 
 const jump = () => {
     farmer.classList.add("pulo")
@@ -8,17 +8,27 @@ const jump = () => {
         farmer.classList.remove("pulo")
     }, 400)
 }
+//colocando animação
+const anim = setInterval(() =>{
+        if (animacao < 2){
+        animacao++
+        farmer.src = `img/farmer_run${animacao}.png`
+        }
+        else{
+            animacao = 1
+            farmer.src = `img/farmer_run${animacao}.png` 
+        }
+        console.log(animacao)
+}, 250  )
+
 const loop = setInterval(() => {
     let fenolado = feno.offsetLeft
-    let farmerlado =+ window.getComputedStyle(farmer).buttom.replace("px", "")
-    
+ //   let farmerlado =+ window.getComputedStyle(farmer).buttom.replace("px", "")
     
 }, 1);
 
-//colocando animação
-const anim = () =>{
 
-}
+
 
 function jogar(){
     window.location.href = "jogo.html"
